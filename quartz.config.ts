a5e632c4266af16a120ddeafe158d3837367fcfc
Ruì Qiū <rexarski@gmail.com>
2024-07-25 21:50:09 -0400

    [UPDATE] update baseurl

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "how2.rexarski.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
