82800cb72556f99ed0e87a8f5abe873f96476db1
Ruì Qiū <rexarski@gmail.com>
2024-07-25 22:09:18 -0400

    [UPDATE] add home page placeholder
    - update locale to zh-CN
    - update footer url

---
+++ content/index.md
@@ -0,0 +1,6 @@
+---
+title: "how2: 如何瞎折腾，试错，以及当一个合格的 homeowner"
+---
+
+This is a blank Quartz installation.
+See the [documentation](https://quartz.jzhao.xyz) for how to get started.
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,13 +8,13 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🏡 how2",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "zh-CN",
     baseUrl: "how2.rexarski.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
